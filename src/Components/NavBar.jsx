1922e288287f1b37de901eb88827a82b6bb8b917
joanavillca <joanavillca@gmail.com>
2025-04-24 19:19:07 -0300

    Agregada barra de búsqueda

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import lakersLogo from '../Assets/lebron.png';
 import './NavBar.css'; 
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const NavBar = () => {
+const [searchTerm, setSearchTerm] = useState("");
+const handleSearchChange = (e) => {
+  setSearchTerm(e.target.value);
+};
+
+const handleSearchSubmit = (e) => {
+  e.preventDefault();
+  console.log('Buscando:', searchTerm);
+};
+
+  
   return (
     <nav className="navbar">
       <div className="nav-links navbar_left">
@@ -20,6 +32,16 @@ const NavBar = () => {
       </div>
 
       <div className="nav-links navbar_right">
+      <form onSubmit={handleSearchSubmit} className="search-form">
+  <input
+    type="text"
+    placeholder="Buscar productos..."
+    value={searchTerm}
+    onChange={handleSearchChange}
+    className="search-input"
+  />
+  <button type="submit" className="search-button">Buscar</button>
+</form>
         <a href="/Login" className="nav-link">Login</a>
         <a href="#carrito" className="nav-link">Carrito (0)</a>
       </div>
